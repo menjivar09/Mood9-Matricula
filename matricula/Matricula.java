8f8a5c33c5c41204fd01cf61776a8e61b5d6bee5
menjivar09 <57577253+menjivar09@users.noreply.github.com>
2019-11-30 23:08:48 -0600

    Add files via upload

---
+++ matricula/Matricula.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matricula;
+
+/**
+ *
+ * @author RUBEN
+ */
+public class Matricula {
+
+  
+    public static void main(String[] args) {
+        vista Vista=new vista();
+        Vista.setVisible(true);
+        
+    }
+    
+}
